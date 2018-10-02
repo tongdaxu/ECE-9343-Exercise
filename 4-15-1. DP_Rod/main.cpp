739aaa853be6701c6239335167467e760b2e27cd
Sock XU <matonglidewazi@qq.com>
2018-10-02 14:33:30 -0400

    HW bug fix, 15-1 Exercise implement

--- 4-15-1. DP_Rod/main.cpp
+++ 4-15-1. DP_Rod/main.cpp
@@ -85,6 +85,43 @@ int memoryRodCut(int n){
     
 }
 
+int memoryRodCutExtAid(int n, std::vector<int> &_memory, std::vector<int> &_choice){
+    //Exercise 15.1-4
+    if (_memory[n]>= 0){
+        return _memory[n];
+    }
+    
+    int q = 0;
+    
+    if (n != 0){
+        q = INT_MIN;
+        
+        for (int i = 1; i <= n; i++){
+            int r = p(i) + memoryRodCutAid (n-i, _memory);
+            _choice[n] = r > q ? i : _choice[n];
+            q = q > r ? q : r;
+        }
+    } //else: pass
+    
+    _memory[n] = q;
+    return q;
+}
+
+void memoryRodCutExt(int n){
+    
+    std::vector<int> memory(n+1, INT_MIN);
+    std::vector<int> choice(n+1, 0);
+    //just to avoid the trouble
+    //write the function separately, or the vector has to be static
+    
+    std::cout<< memoryRodCutExtAid(n, memory, choice) <<std::endl;
+    while (n > 0){
+        std::cout<<choice[n]<<std::endl;
+        n = n - choice[n];
+    }
+    
+}
+
 int upRodCut(int n){
     std::vector<int> memory;
     memory.push_back(0);
@@ -102,6 +139,31 @@ int upRodCut(int n){
     return memory[n];
 }
 
+int upRodCutCost(int n, int c){
+    //Exercise 15.1-3, cutting with cost c
+    std::vector<int> memory;
+    memory.push_back(0);
+    
+    for (int i = 1; i <= n; i++){
+        int q = INT_MIN;
+        for (int j = 1; j <= i; j++){
+            
+            int r;
+            
+            if (i-j == 0){
+               r = p(j) + memory[i-j]; 
+            } else {
+               r = p(j) + memory[i-j] - c;
+            }
+            
+            q = q > r ? q : r;
+        }
+        
+        //memory[i] = q
+        memory.push_back(q);
+    }
+    return memory[n];
+}
 
 std::vector<std::vector<int>> upRodCutExt(int n){
     
@@ -134,13 +196,49 @@ void printRod(int n){
     }
 }
 
+int upFibnocci(int n){
+    //Fibnocci in O(n)
+    std::vector<int> memory (n+1, 0);
+    //0 is unused, with memory[n] exist
+    int i = 1;
+    
+    while (i <= n){
+        
+        if (i == 1){
+            memory[i] = 1;
+        } else if (n == 2){
+            memory[i] = 1;
+        } else {
+            memory[i] = memory[i-1] + memory[i-2];
+        }
+        
+        i++;
+    }
+    
+    return memory[n];
+}
+
+
 //return unordered_map::end in O(1) time 
 
 int main()
 {
     //std::cout<<cutRod(10)<<std::endl;
-    //std::cout<<upRodCut(10)<<std::endl;
+    
+    std::cout<<upRodCutCost(9, 1)<<std::endl;
+    //cutting with cost
+    
+    std::cout<<upRodCut(9)<<std::endl;
+    //cutting up to top
+    
     printRod(9);
+    //cutting with result print
+    
+    memoryRodCutExt(10);
+    //cutting with memory result print
+    
+    std::cout<<upFibnocci(30)<<std::endl;
+    
     return 0;
 }
 

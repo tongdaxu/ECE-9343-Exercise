a7c8bd7a7f720e06812f36afe3f3a06b35e1f55c
Sock XU <matonglidewazi@qq.com>
2018-10-02 11:48:15 -0400

    DP1 Rod cutting

--- 4-15-1. DP_Rod/main.cpp
+++ 4-15-1. DP_Rod/main.cpp
@@ -39,9 +39,7 @@ int cutRod (int n){
     
     if (n == 0){
         return 0;
-    } 
-    
-    //else :
+    } //else
     
     int q = INT_MIN;
     
@@ -55,15 +53,94 @@ int cutRod (int n){
     
 }
 
+
+int memoryRodCutAid(int n, std::vector<int> &_memory){
+    
+    if (_memory[n]>= 0){
+        return _memory[n];
+    }
+    
+    int q = 0;
+    
+    if (n != 0){
+        q = INT_MIN;
+        
+        for (int i = 1; i <= n; i++){
+            int r = p(i) + memoryRodCutAid (n-i, _memory);
+            q = q > r ? q : r;
+        }
+    } //else: pass
+    
+    _memory[n] = q;
+    return q;
+}
+
+int memoryRodCut(int n){
+    
+    std::vector<int> memory(n+1, INT_MIN);
+    //just to avoid the trouble
+    //write the function separately, or the vector has to be static
+    
+    return memoryRodCutAid(n, memory);
+    
+}
+
+int upRodCut(int n){
+    std::vector<int> memory;
+    memory.push_back(0);
+    
+    for (int i = 1; i <= n; i++){
+        int q = INT_MIN;
+        for (int j = 1; j <= i; j++){
+            int r = p(j) + memory[i-j];
+            q = q > r ? q : r;
+        }
+        
+        //memory[i] = q
+        memory.push_back(q);
+    }
+    return memory[n];
+}
+
+
+std::vector<std::vector<int>> upRodCutExt(int n){
+    
+    std::vector<std::vector<int>> result; 
+    std::vector<int> memory (n+1, 0);
+    std::vector<int> choice (n+1, 0);
+    
+    for (int i = 1; i <= n; i++){
+        int q = INT_MIN;
+        for (int j = 1; j <= i; j++){
+            int r = p(j) + memory[i-j];
+            choice[i] = q < r ? j : choice[i];
+            q = q < r ? r : q;
+        }
+        
+        //memory[i] = q
+        memory[i] = q;
+    }
+    
+    result.push_back(memory);
+    result.push_back(choice);
+    return result;
+}
+
+void printRod(int n){
+    std::vector<std::vector<int>> getResult = upRodCutExt(n);
+    while (n > 0){
+        std::cout<<getResult[1][n]<<std::endl;
+        n = n - getResult[1][n];
+    }
+}
+
+//return unordered_map::end in O(1) time 
+
 int main()
 {
-    std::cout<<cutRod(10)<<std::endl;
-    
-    
-    
-    
-    printf("Hello World");
-
+    //std::cout<<cutRod(10)<<std::endl;
+    //std::cout<<upRodCut(10)<<std::endl;
+    printRod(9);
     return 0;
 }
 
